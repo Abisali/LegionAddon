67da81e58bf35147897d82ea26e48f354445ac05
LordMonteven <mboentertainments@gmail.com>
2014-02-22 21:32:11 +0000

    Adding basic Legion Addon

---
+++ Legion.lua
@@ -0,0 +1,34 @@
+--------------------------------------
+--		 Legion Guild Addon			--
+--									--
+--		     Website:				--
+--									--
+--		  legion-hq.com				--
+--									--
+--									--
+--------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+local counter = 1
+ 
+function MyFirstAddOnUpdate()
+    MyFirstAddonCounter:SetText(string.format("Counter: %d", counter))
+    counter = counter + 1
+end
+ 
+function MyFirstAddOnReset()
+    counter = 0
+end
+
+LG = {}
